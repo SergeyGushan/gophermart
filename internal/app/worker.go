e86333952544874f2637a272086b1fb8b4b9246f
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2024-02-29 15:05:10 +0200

    fix

--- internal/app/worker.go
+++ internal/app/worker.go
@@ -3,12 +3,13 @@ package app
 import "time"
 
 func (a *App) RunWorker() error {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
 
-	for range ticker.C {
-		a.worker.Accrual()
-	}
-
+		for range ticker.C {
+			a.worker.Accrual()
+		}
+	}()
 	return nil
 }
